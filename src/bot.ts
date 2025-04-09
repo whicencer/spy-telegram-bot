eedb16fd8cf266d9b92f312d3149193f58cdaeba
Deniel <denielaz2020@gmail.com>
2025-04-09 20:02:05 +0200

    feat: enhance message editing handling with error management and notification formatting

--- src/bot.ts
+++ src/bot.ts
@@ -2,10 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { Bot, Context } from "grammy";
+import dedent from "dedent";
 import { getEnvVariable } from "./utils/getEnvVariable";
 import { UserRepository, IUserRepository } from "../database/User";
 import { IMessagesRepository, MessagesRepository } from "../database/Messages";
-import dedent from "dedent";
+import { TELEGRAM_MAX_MESSAGE_LENGTH } from "./constants/telegram";
 
 export default class BotInstance {
   private bot: Bot = new Bot(getEnvVariable("BOT_TOKEN"));
@@ -45,26 +46,55 @@ export default class BotInstance {
 	}
 
 	private businessEditedMessageHandler = async (ctx: Context) => {
-		const businessConnectionId = ctx.editedBusinessMessage?.business_connection_id;
+		try {
+			const businessConnectionId = ctx.editedBusinessMessage?.business_connection_id;
 
-		if (businessConnectionId && ctx.editedBusinessMessage) {
-			const { user_chat_id: receiverId } = await ctx.api.getBusinessConnection(businessConnectionId);
-			const { message_id, text: newMessageText } = ctx.editedBusinessMessage;
-			const oldMessage = await this.messagesCollection.getById(message_id);
+			if (businessConnectionId && ctx.editedBusinessMessage) {
+				const { user_chat_id: receiverId } = await ctx.api.getBusinessConnection(businessConnectionId);
+				const { message_id, text: newMessageText } = ctx.editedBusinessMessage;
+				const oldMessage = await this.messagesCollection.getById(message_id);
 
-			if (newMessageText) {
-				await this.messagesCollection.messageEdited(message_id, oldMessage.text, newMessageText);
-	
-				const notifyUserMessage = dedent`
-				*User ${ctx.from?.first_name} edited message:*
-				
-				*Old message:*\n\`${oldMessage.text}\`
-	
-				*New message:*\n\`${newMessageText}\`
-				`;
-	
-				await ctx.api.sendMessage(receiverId, notifyUserMessage, { parse_mode: "Markdown" });
+				if (newMessageText) {
+					await this.messagesCollection.messageEdited(message_id, oldMessage.text, newMessageText);
+		
+					const notifyUserMessage = dedent`
+					<b>User ${ctx.from?.first_name} edited message:</b>
+					
+					<b>Old message:</b>
+					<blockquote expandable>${oldMessage.text}</blockquote>
+		
+					<b>New message:</b>
+					<blockquote expandable>${newMessageText}</blockquote>
+					`;
+		
+					if (notifyUserMessage.length > TELEGRAM_MAX_MESSAGE_LENGTH) {
+						await ctx.api.sendMessage(
+							receiverId,
+							dedent`
+								<b>User ${ctx.from?.first_name} edited message:</b>\n
+								Message cannot be displayed, because it exceeds the limit of 4096 characters.
+							`,
+							{ parse_mode: "HTML" }
+						);
+					} else {
+						await ctx.api.sendMessage(receiverId, notifyUserMessage, { parse_mode: "HTML" });
+					}
+				}
 			}
+		} catch (error: any) {
+			const { chat_id, parse_mode } = error.payload;
+			const errorDescription = error.description;
+
+			if (chat_id) {
+				switch (errorDescription) {
+					case "Bad Request: message is too long":
+						await ctx.api.sendMessage(chat_id, "Error: limit of 4096 characters exceeded", { parse_mode });
+						break;
+					default:
+						await ctx.api.sendMessage(chat_id, "Неизвестная ошибка", { parse_mode });
+				}
+			}
+			console.error("Error in businessEditedMessageHandler:", error.description);
 		}
 	}
 
---
+++ src/constants/telegram.ts
@@ -0,0 +1,1 @@
+export const TELEGRAM_MAX_MESSAGE_LENGTH = 4096;
