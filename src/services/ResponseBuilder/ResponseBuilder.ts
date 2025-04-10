08fdb3c0e66a5ff3503a8ac8ab8a2a8bb8572add
Deniel <denielaz2020@gmail.com>
2025-04-10 03:21:36 +0200

    refactor: streamline message response creation by consolidating logic into createMessage method

--- src/services/ResponseBuilder/ResponseBuilder.ts
+++ src/services/ResponseBuilder/ResponseBuilder.ts
@@ -1,11 +1,39 @@
 import dedent from "dedent";
 import { ParseMode, User } from "grammy/types";
 
+enum ActionTypes {
+  DELETED = "deleted",
+  EDITED = "edited"
+}
+
 export class ResponseBuilder {
   private static TELEGRAM_MAX_MESSAGE_LENGTH = 4096;
 
+  private static createMessage(
+    from: User,
+    messageContent: string,
+    actionType: ActionTypes
+  ): { text: string; parse_mode: ParseMode } {
+    const message = messageContent;
+
+    if (message.length > this.TELEGRAM_MAX_MESSAGE_LENGTH) {
+      return {
+        text: dedent`
+          <b>User <a href="https://t.me/${from.username}">${from.first_name}</a> (<i>ID <code>${from.id}</code></i>) ${actionType} message:</b>\n
+          <i>Message cannot be displayed, because it exceeds the limit of 4096 characters.</i>
+        `,
+        parse_mode: "HTML"
+      }
+    }
+
+    return {
+      text: message,
+      parse_mode: "HTML"
+    }
+  }
+
   public static buildEditedMessageResponse(from: User, oldMessageText: string, newMessageText: string): { text: string; parse_mode: ParseMode } {
-    const message = dedent`
+    const messageContent = dedent`
       <b>User <a href="https://t.me/${from.username}">${from.first_name}</a> (<i>ID <code>${from.id}</code></i>) edited message:</b>\n
       
       <b>Old message:</b>
@@ -14,19 +42,16 @@ export class ResponseBuilder {
       <b>New message:</b>
       <blockquote expandable>${newMessageText}</blockquote>
     `;
-    if (message.length > this.TELEGRAM_MAX_MESSAGE_LENGTH) {
-      return {
-        text: dedent`
-          <b>User <a href="https://t.me/${from.username}">${from.first_name}</a>(<i>ID <code>${from.id}</code></i>) edited message:</b>\n
-          <i>Message cannot be displayed, because it exceeds the limit of 4096 characters.</i>
-        `,
-        parse_mode: "HTML",
-      };
-    }
+    
+    return this.createMessage(from, messageContent, ActionTypes.EDITED);
+  }
 
-    return {
-      text: message,
-      parse_mode: "HTML",
-    };
+  public static buildDeletedMessageResponse(from: User, messageText: string): { text: string; parse_mode: ParseMode } {
+    const messageContent = dedent`
+      <b>User <a href="https://t.me/${from.username}">${from.first_name}</a> (<i>ID <code>${from.id}</code></i>) deleted message:</b>\n
+      <blockquote expandable>${messageText}</blockquote>
+    `;
+
+    return this.createMessage(from, messageContent, ActionTypes.DELETED);
   }
 }
